b35f27985e4a18d66c69c68a96853c1e6a512850
leonardomontero <leonardomontero@brq.com>
2020-05-08 10:49:03 -0300

    finish

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 from src import Mongo, Carros
 
 from flask import Flask, request, jsonify
-from functools import wraps
 
 db = Mongo.Mongo("carros", "carros")
 app = Flask(__name__)
@@ -40,4 +39,4 @@ def healthcheck():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port='8081')
